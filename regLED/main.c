123b55692d783fba213a769d1d5086985b43e2c1
GreenBird12 <ly2310159477@163.com>
2022-11-25 22:00:57 +0800

    ly1

--- regLED/main.c
+++ regLED/main.c
@@ -16,6 +16,13 @@ void SystemInit(void);
 #define RED_LED RED_PORT, RED_PIN
 
 #define GREEN_PIN GPIO_Pin8
+#define GREEN_PORT GPIOC
+#define GREEN_LED GREEN_PORT, GREEN_PIN
+
+#define BLUE_PIN GPIO_Pin7
+#define BLUE_PORT GPIOC
+#define BLUE_LED BLUE_PORT, BLUE_PIN
+
 void GPIO_flip(GPIO_StructTypedef *gpioN, uint16_t pin);
 void GPIO_flip(GPIO_StructTypedef *gpioN, uint16_t pin)
 {
@@ -24,7 +31,7 @@ void GPIO_flip(GPIO_StructTypedef *gpioN, uint16_t pin)
         GPIO_Resetbit(gpioN, pin);
     else
         GPIO_Setbit(gpioN, pin);
-
+        
     // 寄存器版本
     // if (gpioN->ODR & pin)
     //     gpioN->BRR = pin;
@@ -40,27 +47,39 @@ int main()
     RCC_AHBENR |= (1 << RCC_AHB_GPIOD_Pos);
 
     // 初始化GPIO
-    g.pin = GPIO_Pin7 | RED_PIN | GREEN_PIN;
+    g.pin = BLUE_PIN | RED_PIN | GREEN_PIN;
     g.mode = GPIO_Mode_pull_push;
     g.speed = GPIO_Speed_50MHz;
     GPIO_Init(GPIOC, &g);
 
-    g.pin = RED_PIN;
-    GPIO_Init(RED_PORT, &g);
 
     g.pin = GPIO_Pin6;
     g.mode = GPIO_Mode_IPU;
     GPIO_Init(GPIOD, &g);
 
+    g.pin = GPIO_Pin11|GPIO_Pin12;
+    GPIO_Init(GPIOC, &g);
     // GPIO_Setbit(GPIOC, GPIO_Pin7 | GPIO_Pin8 | GPIO_Pin6);
 
     while (1)
     {
-
-        if (GPIO_readInputDataBit(GPIOD, GPIO_Pin6))
+//不按是高电平，按下才是低电平
+        if (!GPIO_readInputDataBit(GPIOD, GPIO_Pin6))
         {
             GPIO_flip(RED_LED);
-            while ((GPIO_readInputDataBit(GPIOD, GPIO_Pin6)))
+            while (!(GPIO_readInputDataBit(GPIOD, GPIO_Pin6)))
+                ;
+        }
+        if (!GPIO_readInputDataBit(GPIOC, GPIO_Pin12))
+        {
+            GPIO_flip(BLUE_LED);
+            while (!(GPIO_readInputDataBit(GPIOC, GPIO_Pin12)))
+                ;
+        }
+        if (!GPIO_readInputDataBit(GPIOC, GPIO_Pin11))
+        {
+            GPIO_flip(GREEN_LED);
+            while (!(GPIO_readInputDataBit(GPIOC, GPIO_Pin11)))
                 ;
         }
     }
